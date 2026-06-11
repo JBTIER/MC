619184f25634adf570534df31b63c35b59b9a6a0
JBTIER <verynextgenai@gmail.com>
2026-06-11 12:36:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,236 +24,37 @@ const gamemodeLabels = {
 };
 
 const players = [
-  {
-    id: 1,
-    username: 'ItzRealMe',
-    avatar: 'assets/players/itzrealme.png',
-    region: 'NA',
-    title: 'Combat Master',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT3', axe: 'LT2', mace: 'LT2', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT1', netherite: 'HT1', uhc: 'LT2' }
-  },
-  {
-    id: 2,
-    username: 'TechnoBlade',
-    avatar: 'assets/players/technoblade.png',
-    region: 'NA',
-    title: 'The Blade',
-    socials: { youtube: 'https://youtube.com/technoblade', twitter: 'https://twitter.com/technoblade', discord: '' },
-    tiers: { sword: 'HT1', axe: 'HT1', mace: 'HT2', crystal: 'LT1', diasmp: 'HT2', smp: 'HT3', pot: 'HT1', netherite: 'HT2', uhc: 'HT1' }
-  },
-  {
-    id: 3,
-    username: 'Dream',
-    avatar: 'assets/players/dream.png',
-    region: 'NA',
-    title: 'Speedrunner',
-    socials: { youtube: 'https://youtube.com/dream', twitter: 'https://twitter.com/dream', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT1', crystal: 'HT2', diasmp: 'LT1', smp: 'HT1', pot: 'HT3', netherite: 'LT1', uhc: 'HT2' }
-  },
-  {
-    id: 4,
-    username: 'Fruitberries',
-    avatar: 'assets/players/fruitberries.png',
-    region: 'NA',
-    title: 'The Mechanic',
-    socials: { youtube: 'https://youtube.com/fruitberries', twitter: '', discord: '' },
-    tiers: { sword: 'LT1', axe: 'HT1', mace: 'HT1', crystal: 'HT3', diasmp: 'HT2', smp: 'HT2', pot: 'LT1', netherite: 'HT2', uhc: 'HT1' }
-  },
-  {
-    id: 5,
-    username: 'Illumina',
-    avatar: 'assets/players/illumina.png',
-    region: 'OCE',
-    title: 'The Speedster',
-    socials: { youtube: 'https://youtube.com/illumina', twitter: 'https://twitter.com/illumina1337', discord: '' },
-    tiers: { sword: 'HT1', axe: 'HT2', mace: 'LT1', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT2', netherite: 'HT2', uhc: 'LT1' }
-  },
-  {
-    id: 6,
-    username: 'Purpled',
-    avatar: 'assets/players/purpled.png',
-    region: 'NA',
-    title: 'The Alien',
-    socials: { youtube: 'https://youtube.com/purpled', twitter: '', discord: '' },
-    tiers: { sword: 'HT1', axe: 'LT1', mace: 'HT1', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT1', netherite: 'HT1', uhc: 'HT2' }
-  },
-  {
-    id: 7,
-    username: 'Sapnap',
-    avatar: 'assets/players/sapnap.png',
-    region: 'NA',
-    title: 'The Brawler',
-    socials: { youtube: 'https://youtube.com/sapnap', twitter: 'https://twitter.com/sapnap', discord: '' },
-    tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT3', crystal: 'LT1', diasmp: 'HT1', smp: 'HT3', pot: 'HT2', netherite: 'LT1', uhc: 'HT3' }
-  },
-  {
-    id: 8,
-    username: 'xQc',
-    avatar: 'assets/players/xqc.png',
-    region: 'NA',
-    title: 'The Juicer',
-    socials: { youtube: '', twitter: 'https://twitter.com/xqc', discord: '' },
-    tiers: { sword: 'LT3', axe: 'LT3', mace: 'HT4', crystal: 'LT3', diasmp: 'LT2', smp: 'LT1', pot: 'LT2', netherite: 'LT3', uhc: 'LT3' }
-  },
-  {
-    id: 9,
-    username: 'Refraction',
-    avatar: 'assets/players/refraction.png',
-    region: 'NA',
-    title: 'The Strategist',
-    socials: { youtube: 'https://youtube.com/refraction', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT2', crystal: 'HT1', diasmp: 'HT1', smp: 'LT1', pot: 'HT3', netherite: 'HT2', uhc: 'HT3' }
-  },
-  {
-    id: 10,
-    username: 'B0MBIES',
-    avatar: 'assets/players/b0mbies.png',
-    region: 'EU',
-    title: 'The Clutch King',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 11,
-    username: 'Stimpee',
-    avatar: 'assets/players/stimpee.png',
-    region: 'NA',
-    title: 'The PvPer',
-    socials: { youtube: 'https://youtube.com/stimpee', twitter: '', discord: '' },
-    tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT1', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 12,
-    username: 'ClownPierce',
-    avatar: 'assets/players/clownpierce.png',
-    region: 'EU',
-    title: 'The Clown',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT1', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 13,
-    username: 'Minikloon',
-    avatar: 'assets/players/minikloon.png',
-    region: 'EU',
-    title: 'The Admin',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT3', axe: 'HT3', mace: 'LT2', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT3', netherite: 'HT3', uhc: 'HT3' }
-  },
-  {
-    id: 14,
-    username: 'TapL',
-    avatar: 'assets/players/tapl.png',
-    region: 'NA',
-    title: 'The Veteran',
-    socials: { youtube: 'https://youtube.com/tapl', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 15,
-    username: 'Vikkstar123',
-    avatar: 'assets/players/vikkstar.png',
-    region: 'EU',
-    title: 'The OG',
-    socials: { youtube: 'https://youtube.com/vikkstar123', twitter: 'https://twitter.com/vikkstar123', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'LT1', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT2', netherite: 'HT3', uhc: 'HT2' }
-  },
-  {
-    id: 16,
-    username: 'PeteZahHutt',
-    avatar: 'assets/players/petezahhutt.png',
-    region: 'NA',
-    title: 'The Champion',
-    socials: { youtube: 'https://youtube.com/petezahhutt', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 17,
-    username: 'Fundy',
-    avatar: 'assets/players/fundy.png',
-    region: 'NA',
-    title: 'The Fox',
-    socials: { youtube: 'https://youtube.com/fundy', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT3', netherite: 'HT3', uhc: 'HT2' }
-  },
-  {
-    id: 18,
-    username: 'TommyInnit',
-    avatar: 'assets/players/tommyinnit.png',
-    region: 'EU',
-    title: 'The Child',
-    socials: { youtube: 'https://youtube.com/tommyinnit', twitter: 'https://twitter.com/tommyinnit', discord: '' },
-    tiers: { sword: 'LT2', axe: 'LT2', mace: 'LT3', crystal: 'LT2', diasmp: 'LT2', smp: 'LT2', pot: 'LT2', netherite: 'LT2', uhc: 'LT3' }
-  },
-  {
-    id: 19,
-    username: 'Quackity',
-    avatar: 'assets/players/quackity.png',
-    region: 'SA',
-    title: 'The CEO',
-    socials: { youtube: 'https://youtube.com/quackity', twitter: 'https://twitter.com/quackity', discord: '' },
-    tiers: { sword: 'LT3', axe: 'LT3', mace: 'LT3', crystal: 'LT3', diasmp: 'LT2', smp: 'LT2', pot: 'LT3', netherite: 'LT3', uhc: 'LT3' }
-  },
-  {
-    id: 20,
-    username: 'Gamerboy80',
-    avatar: 'assets/players/gamerboy80.png',
-    region: 'NA',
-    title: 'The Bedrock',
-    socials: { youtube: 'https://youtube.com/gamerboy80', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'LT2', diasmp: 'LT1', smp: 'LT1', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 21,
-    username: 'Manhal',
-    avatar: 'assets/players/manhal.png',
-    region: 'ASIA',
-    title: 'The Demon',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT1', axe: 'LT1', mace: 'HT1', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT1', netherite: 'HT1', uhc: 'HT1' }
-  },
-  {
-    id: 22,
-    username: 'Danteh',
-    avatar: 'assets/players/danteh.png',
-    region: 'NA',
-    title: 'The Flex',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' }
-  },
-  {
-    id: 23,
-    username: 'Krusty',
-    avatar: 'assets/players/krusty.png',
-    region: 'OCE',
-    title: 'The Underdog',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'LT1', axe: 'LT1', mace: 'LT1', crystal: 'LT1', diasmp: 'LT1', smp: 'LT2', pot: 'LT1', netherite: 'LT1', uhc: 'LT1' }
-  },
-  {
-    id: 24,
-    username: 'BaboAbe',
-    avatar: 'assets/players/baboabe.png',
-    region: 'ASIA',
-    title: 'The Rising Star',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT3', axe: 'HT3', mace: 'HT3', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT3', netherite: 'HT3', uhc: 'HT3' }
-  },
-  {
-    id: 25,
-    username: 'Deadlox',
-    avatar: 'assets/players/deadlox.png',
-    region: 'NA',
-    title: 'The Ghost',
-    socials: { youtube: '', twitter: '', discord: '' },
-    tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT2', crystal: 'LT2', diasmp: 'LT2', smp: 'LT3', pot: 'HT2', netherite: 'HT2', uhc: 'LT2' }
-  }
+  { id: 1, username: 'ItzRealMe', region: 'NA', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT3', axe: 'LT2', mace: 'LT2', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT1', netherite: 'HT1', uhc: 'LT2' } },
+  { id: 2, username: 'TechnoBlade', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/technoblade', twitter: 'https://twitter.com/technoblade', discord: '' }, tiers: { sword: 'HT1', axe: 'HT1', mace: 'HT2', crystal: 'LT1', diasmp: 'HT2', smp: 'HT3', pot: 'HT1', netherite: 'HT2', uhc: 'HT1' } },
+  { id: 3, username: 'Dream', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/dream', twitter: 'https://twitter.com/dream', discord: '' }, tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT1', crystal: 'HT2', diasmp: 'LT1', smp: 'HT1', pot: 'HT3', netherite: 'LT1', uhc: 'HT2' } },
+  { id: 4, username: 'Fruitberries', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/fruitberries', twitter: '', discord: '' }, tiers: { sword: 'LT1', axe: 'HT1', mace: 'HT1', crystal: 'HT3', diasmp: 'HT2', smp: 'HT2', pot: 'LT1', netherite: 'HT2', uhc: 'HT1' } },
+  { id: 5, username: 'Illumina', region: 'OCE', platform: 'Java', socials: { youtube: 'https://youtube.com/illumina', twitter: 'https://twitter.com/illumina1337', discord: '' }, tiers: { sword: 'HT1', axe: 'HT2', mace: 'LT1', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT2', netherite: 'HT2', uhc: 'LT1' } },
+  { id: 6, username: 'Purpled', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/purpled', twitter: '', discord: '' }, tiers: { sword: 'HT1', axe: 'LT1', mace: 'HT1', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT1', netherite: 'HT1', uhc: 'HT2' } },
+  { id: 7, username: 'Sapnap', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/sapnap', twitter: 'https://twitter.com/sapnap', discord: '' }, tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT3', crystal: 'LT1', diasmp: 'HT1', smp: 'HT3', pot: 'HT2', netherite: 'LT1', uhc: 'HT3' } },
+  { id: 8, username: 'xQc', region: 'NA', platform: 'Java', socials: { youtube: '', twitter: 'https://twitter.com/xqc', discord: '' }, tiers: { sword: 'LT3', axe: 'LT3', mace: 'HT4', crystal: 'LT3', diasmp: 'LT2', smp: 'LT1', pot: 'LT2', netherite: 'LT3', uhc: 'LT3' } },
+  { id: 9, username: 'Refraction', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/refraction', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT2', crystal: 'HT1', diasmp: 'HT1', smp: 'LT1', pot: 'HT3', netherite: 'HT2', uhc: 'HT3' } },
+  { id: 10, username: 'B0MBIES', region: 'EU', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 11, username: 'Stimpee', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/stimpee', twitter: '', discord: '' }, tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT1', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 12, username: 'ClownPierce', region: 'EU', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT1', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 13, username: 'Minikloon', region: 'EU', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT3', axe: 'HT3', mace: 'LT2', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT3', netherite: 'HT3', uhc: 'HT3' } },
+  { id: 14, username: 'TapL', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/tapl', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 15, username: 'Vikkstar123', region: 'EU', platform: 'Java', socials: { youtube: 'https://youtube.com/vikkstar123', twitter: 'https://twitter.com/vikkstar123', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'LT1', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT2', netherite: 'HT3', uhc: 'HT2' } },
+  { id: 16, username: 'PeteZahHutt', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/petezahhutt', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 17, username: 'Fundy', region: 'NA', platform: 'Java', socials: { youtube: 'https://youtube.com/fundy', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT3', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT3', netherite: 'HT3', uhc: 'HT2' } },
+  { id: 18, username: 'TommyInnit', region: 'EU', platform: 'Java', socials: { youtube: 'https://youtube.com/tommyinnit', twitter: 'https://twitter.com/tommyinnit', discord: '' }, tiers: { sword: 'LT2', axe: 'LT2', mace: 'LT3', crystal: 'LT2', diasmp: 'LT2', smp: 'LT2', pot: 'LT2', netherite: 'LT2', uhc: 'LT3' } },
+  { id: 19, username: 'Quackity', region: 'SA', platform: 'Java', socials: { youtube: 'https://youtube.com/quackity', twitter: 'https://twitter.com/quackity', discord: '' }, tiers: { sword: 'LT3', axe: 'LT3', mace: 'LT3', crystal: 'LT3', diasmp: 'LT2', smp: 'LT2', pot: 'LT3', netherite: 'LT3', uhc: 'LT3' } },
+  { id: 20, username: 'Gamerboy80', region: 'NA', platform: 'Bedrock', socials: { youtube: 'https://youtube.com/gamerboy80', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'LT2', diasmp: 'LT1', smp: 'LT1', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 21, username: 'Manhal', region: 'ASIA', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT1', axe: 'LT1', mace: 'HT1', crystal: 'HT1', diasmp: 'HT1', smp: 'HT1', pot: 'HT1', netherite: 'HT1', uhc: 'HT1' } },
+  { id: 22, username: 'Danteh', region: 'NA', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT2', axe: 'HT2', mace: 'HT2', crystal: 'HT2', diasmp: 'HT2', smp: 'HT2', pot: 'HT2', netherite: 'HT2', uhc: 'HT2' } },
+  { id: 23, username: 'Krusty', region: 'OCE', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'LT1', axe: 'LT1', mace: 'LT1', crystal: 'LT1', diasmp: 'LT1', smp: 'LT2', pot: 'LT1', netherite: 'LT1', uhc: 'LT1' } },
+  { id: 24, username: 'BaboAbe', region: 'ASIA', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT3', axe: 'HT3', mace: 'HT3', crystal: 'HT3', diasmp: 'HT3', smp: 'HT3', pot: 'HT3', netherite: 'HT3', uhc: 'HT3' } },
+  { id: 25, username: 'Deadlox', region: 'NA', platform: 'Java', socials: { youtube: '', twitter: '', discord: '' }, tiers: { sword: 'HT1', axe: 'HT2', mace: 'HT2', crystal: 'LT2', diasmp: 'LT2', smp: 'LT3', pot: 'HT2', netherite: 'HT2', uhc: 'LT2' } }
 ];
 
 let currentGamemode = 'overall';
 let currentSort = 'points-desc';
 let searchQuery = '';
+let currentPlatform = 'all';
 
 function calculatePoints(player) {
   return Object.values(player.tiers).reduce((total, tier) => {
@@ -268,6 +69,11 @@ function getAvatarHtml(player, size = 36) {
 function getRegionBadge(region) {
   const cls = region.toLowerCase();
   return `<span class="region-badge ${cls}">${region}</span>`;
+}
+
+function getPlatformBadge(platform) {
+  const icon = platform === 'Java' ? 'fa-cube' : 'fa-mobile-alt';
+  return `<span class="platform-badge ${platform.toLowerCase()}"><i class="fas ${icon}"></i> ${platform}</span>`;
 }
 
 function getRankBadge(index) {
@@ -287,6 +93,10 @@ function getTierHtml(tiers) {
 
 function getFilteredAndSortedPlayers() {
   let result = [...players];
+
+  if (currentPlatform !== 'all') {
+    result = result.filter(p => p.platform === currentPlatform);
+  }
 
   if (searchQuery) {
     const q = searchQuery.toLowerCase();
@@ -352,7 +162,7 @@ function renderLeaderboard() {
           </div>
         </td>
         <td>${getRegionBadge(player.region)}</td>
-        <td><span class="title-text">${player.title}</span></td>
+        <td>${getPlatformBadge(player.platform)}</td>
         <td><span class="points-text">${points}</span></td>
         <td>${tierDisplay}</td>
       </tr>
@@ -395,7 +205,7 @@ function openModal(playerId) {
   body.innerHTML = `
     <img class="modal-avatar" src="https://mc-heads.net/avatar/${player.username}/80" alt="${player.username}" loading="lazy" onerror="this.style.display='none';this.nextElementSibling.style.display='flex'"><div class="modal-avatar fallback" style="display:none">${player.username[0].toUpperCase()}</div>
     <h2 class="modal-username">${player.username}</h2>
-    <p class="modal-title">${player.title} - ${getRegionBadge(player.region)}</p>
+    <p class="modal-title">${getPlatformBadge(player.platform)} ${getRegionBadge(player.region)}</p>
     <div class="modal-meta">
       <div class="modal-meta-item">
         <span class="modal-meta-value">#${rank}</span>
@@ -472,6 +282,16 @@ document.addEventListener('DOMContentLoaded', () => {
   sortSelect.addEventListener('change', (e) => {
     currentSort = e.target.value;
     renderLeaderboard();
+  });
+
+  const platformTabs = document.querySelectorAll('.platform-tab');
+  platformTabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      platformTabs.forEach(t => t.classList.remove('active'));
+      tab.classList.add('active');
+      currentPlatform = tab.dataset.platform;
+      renderLeaderboard();
+    });
   });
 
   const modalOverlay = document.getElementById('modalOverlay');
